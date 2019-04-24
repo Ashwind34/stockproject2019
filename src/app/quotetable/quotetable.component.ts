1ffec1365a5484e8afda7c12da9e0bb8479224d9
Ashwind34 <ahester34@gmail.com>
2019-04-23 18:01:16 -0700

    quote data table working with vertical

--- src/app/quotetable/quotetable.component.ts
+++ src/app/quotetable/quotetable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { headersToString } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-quotetable',
@@ -13,7 +14,20 @@ export class QuotetableComponent implements OnInit {
 
   headers = ['Symbol', 'Price',  'Open',  'High',  'Low', 'Volume', 'Date', 'Prev. Close', 'Change ($)', 'Change (%)'];
 
+  fullData = [];
+
+  createTableData() {
+    for (let i = 0; i < this.headers.length; i++ ) {
+      let item = {
+        header: this.headers[i],
+        data: this.quoteData[i]
+      }
+      this.fullData.push(item)
+    }
+  }
+
   ngOnInit() {
+    this.createTableData()
   }
 
 }
